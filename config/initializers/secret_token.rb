3006fa9d196b886cefbb7d3244dac916cb6ad479
root <root@ZBWARTCC.MCN.ION.COM>
2011-09-21 16:29:12 -0400

    First version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OsipsSnoop::Application.config.secret_token = '0bffcc8f69d5ae586c5c8f111c257edc2377ef6271c115d2a5c36b9c9f0902dc4f389d2e0cd5588b7d09dea2ef8afdc00dc5848c8f6c31beb0dce310c43d440e'
